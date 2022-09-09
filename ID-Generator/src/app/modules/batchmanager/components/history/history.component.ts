bedba955f751755dd13dc03aac307b1f150b6877
sreyasag <sreyasag@gmail.com>
2022-09-09 17:32:26 +0530

    xlsx and pdf create

--- ID-Generator/src/app/modules/batchmanager/components/history/history.component.ts
+++ ID-Generator/src/app/modules/batchmanager/components/history/history.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { jsPDF } from "jspdf";
+import autoTable from 'jspdf-autotable';
+
 import { ApplicationService } from 'src/app/modules/core/services/application.service';
 import { AuthService } from 'src/app/modules/core/services/auth.service';
+import * as XLSX from 'xlsx';
+
+
 
 @Component({
   selector: 'app-history',
@@ -19,6 +25,7 @@ export class HistoryComponent implements OnInit {
   fromDate:any 
   toDate:any 
   showTable=false;
+  historyData:any;
 
   dataSource:any;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -28,7 +35,6 @@ export class HistoryComponent implements OnInit {
     '_id',
     'createdAt',
     'status',
-    'student_id',
   ];
 
 
@@ -47,7 +53,7 @@ export class HistoryComponent implements OnInit {
     this.applications.getHistory(this.batchesString,from,to)
     .subscribe({ 
       next: (data:any)=> {
-        console.log(data);
+        this.historyData=data;
         this.dataSource = new MatTableDataSource<any>(data);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
@@ -58,4 +64,38 @@ export class HistoryComponent implements OnInit {
       }
     })
   }
+
+  //excel
+  //@ViewChild('TABLE') table!: ElementRef;
+  createExcel()
+  {
+    const ws: XLSX.WorkSheet=XLSX.utils.json_to_sheet(this.historyData) //table_to_sheet(this.table.nativeElement);//converts a DOM TABLE element to a worksheet
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    /* save to file */
+    XLSX.writeFile(wb, 'SheetJS.xlsx');
+  }
+
+ 
+  //pdf
+  createPdf(){
+    const doc = new jsPDF();
+    const col = ['_id', 'createdAt', 'status'];
+    const rows:any = [];
+    
+    const itemNew = this.historyData;
+    
+    itemNew.forEach((element: { _id: any; createdAt: any; status: any; }) => {
+      const temp = [element._id, element.createdAt, element.status];
+      rows.push(temp);
+    });
+    
+    autoTable(doc, {
+      head: [col],
+      body: rows,
+    })
+    
+    doc.save('table.pdf')
+  }
+
 }
