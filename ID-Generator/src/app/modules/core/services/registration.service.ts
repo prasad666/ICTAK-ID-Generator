5e2926ea9e22ba52deb2911a469901af2e607ebc
Prasad P N <prasad.p@dalason.com>
2022-09-09 11:20:37 +0530

    Fixed miissing import http in registration servite

--- ID-Generator/src/app/modules/core/services/registration.service.ts
+++ ID-Generator/src/app/modules/core/services/registration.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { environment } from 'environments/environment';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
-  providedIn: 'any'
+  providedIn: 'any',
 })
-
 export class RegistrationService {
-
+  constructor(private http: HttpClient) {}
   newStudent(item: any) {
     return this.http
       .post(environment.api_url + '/insert', { student: item })
